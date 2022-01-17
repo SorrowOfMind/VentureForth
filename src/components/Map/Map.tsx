9c64f7923adc6e95deb5f16ede3d5610e0a38389
SorrowOfMind <sortoflifecraze@gmail.com>
2022-01-17 22:30:56 +0100

    structure

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Map = (): JSX.Element =>  {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Map
---
+++ src/components/TopBar/TopBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopBar = (): JSX.Element => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TopBar;
