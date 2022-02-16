49d58d9e97c84fb6a80fce22039610dcbff38504
SorrowOfMind <sortoflifecraze@gmail.com>
2022-02-16 20:58:40 +0100

    topbar

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import {Autocomplete} from '@react-google-maps/api';
+import SearchIcon from '@material-ui/icons/Search';
+import {AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core';
+import useStyles from './style';
 
 const TopBar = (): JSX.Element => {
+    const classes = useStyles();
     return (
-        <div>
-            <h1>top</h1>
-        </div>
+        <AppBar position="static" className={classes.header}>
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    VentureForth
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className={classes.title}>
+                        your personal travel advisor
+                    </Typography>
+                    {/* <Autocomplete> */}
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="search..." classes={{root: classes.inputRoot, input: classes.inputInput}} />
+                        </div>
+                    {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
 
